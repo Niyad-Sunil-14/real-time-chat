a512627a0d4c15ebff9be7676d99fd7cb528e05a
Niyad <niyadsystem@gmail.com>
2024-11-03 17:00:56 +0530

    testing1

--- a_core/settings.py
+++ a_core/settings.py
@@ -32,9 +32,9 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'real-time-chat-wkap.onrender.com']
 
-CSRF_TRUSTED_ORIGINS = [ 'https://*' ]
+CSRF_TRUSTED_ORIGINS = [ 'https://real-time-chat-wkap.onrender.com' ]
 
 # Application definition
 
